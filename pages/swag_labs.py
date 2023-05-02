ed02d275ec4320f8ad916923abde745d496b3649
timatidallas <dallas_84@mail.ru>
2023-05-02 20:16:12 +0300

    home_commit

--- pages/swag_labs.py
+++ pages/swag_labs.py
@@ -3,7 +3,7 @@ from pages.base_page import BasePage
 
 
 class SwagLabs(BasePage):
-    def exit_icon(self):
+    def exist_icon(self):
         try:
             self.find_element(locator='div.login_logo')
         except NoSuchElementException:
@@ -13,3 +13,16 @@ class SwagLabs(BasePage):
         if self.get_url() == 'https://www.saucedemo.com/':
             return True
         return False
+    def exist_user_name(self):
+        try:
+            self.find_element(locator='#user-name')
+        except NoSuchElementException:
+            return False
+        return True
+
+    def exist_password(self):
+        try:
+            self.find_element(locator='#password')
+        except NoSuchElementException:
+            return False
+        return True
--- tests/test_check_swag.py
+++ tests/test_check_swag.py
@@ -2,6 +2,7 @@ from pages.swag_labs import SwagLabs
 def test_icon_exist(browser):
     demo_qa_page = SwagLabs(browser)
     demo_qa_page.visit()
-    demo_qa_page.exit_icon()
-    assert demo_qa_page.exit_icon()
+    assert demo_qa_page.exist_icon()
     assert demo_qa_page.equal_url()
+    assert demo_qa_page.exist_user_name()
+    assert demo_qa_page.exist_password()
